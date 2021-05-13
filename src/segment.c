e45b83f6f6bd4effcb5d96d467a53d9e5a05f081
Damien <thecsmine@gmail.com>
2021-05-13 19:44:06 +0200

    modify add_pixels

--- src/segment.c
+++ src/segment.c
@@ -13,7 +13,6 @@ ei_color_t pixel_to_rgba(ei_surface_t surface, uint32_t pixel) {
 
 	/* Obtenir les indices et ranger dans un tableau */
 	hw_surface_get_channel_indices(surface, &ir, &ig, &ib, &ia);
-	// uint32_t rgba = array[3] * 16777216 + array[2] * 65536 + array[1] * 256 + array[0];
 	int array[4] = {0, 8, 16, 24};
 	color.red = (unsigned char) (pixel >> array[ir]) & 0x000000FF;
 	color.green = (unsigned char) (pixel >> array[ig]) & 0x000000FF;
@@ -44,31 +43,30 @@ void draw_pixel(ei_surface_t surface, uint32_t *pixel_ptr, int x, int y, ei_colo
 		if (color == NULL) {
 			*pixel_ptr = (uint32_t) 0x00000000;
 		} else {
-			*pixel_ptr = add_pixels(surface, NULL, color, pixel_ptr, alpha);
+			*pixel_ptr = add_pixels(surface, NULL, color, surface, pixel_ptr, alpha);
 		}
 	}
 }
 
-uint32_t add_pixels(ei_surface_t surface, uint32_t *src_pixel, ei_color_t *src_color, uint32_t *dst_pixel, ei_bool_t alpha) {
+uint32_t add_pixels(ei_surface_t source, uint32_t *src_pixel, ei_color_t *src_color, ei_surface_t destination, uint32_t *dst_pixel, ei_bool_t alpha) {
 	uint32_t result;
 	if (alpha) { // Use additive transparency
 		ei_color_t src;
-		ei_color_t dst = pixel_to_rgba(surface, *dst_pixel);
+		ei_color_t dst = pixel_to_rgba(destination, *dst_pixel);
 		if (src_color == NULL) {
-			src = pixel_to_rgba(surface, *src_pixel);
+			src = pixel_to_rgba(source, *src_pixel);
 		} else {
 			src = *src_color;
 		}
 		dst.red = (src.alpha*src.red + (255 - src.alpha)*dst.red)/255;
 		dst.green = (src.alpha*src.green + (255 - src.alpha)*dst.green)/255;
 		dst.blue = (src.alpha*src.blue + (255 - src.alpha)*dst.blue)/255;
-		dst.alpha = 255;
 
-		return ei_map_rgba(surface, dst);
+		return ei_map_rgba(destination, dst);
 
 	} else { // Copy source to result
 		if (src_pixel == NULL) {
-			result = ei_map_rgba(surface, *src_color);
+			result = ei_map_rgba(source, *src_color);
 		} else {
 			result = *src_pixel;
 		}
@@ -80,7 +78,6 @@ void draw_segment_straight(ei_surface_t surface,
 			   int x1, int x2, int y1, int y2,
 			   ei_color_t color,
 			   const ei_rect_t *clipper) {
-	/* TODO: Transparence additionnée */
 	int width = hw_surface_get_size(surface).width;
 	int i, sign = 1, incr;
 	uint32_t *pixel_ptr = (uint32_t *) hw_surface_get_buffer(surface);
@@ -121,7 +118,6 @@ void draw_segment_bresenham(ei_surface_t surface,
 			    int x1, int y1, int dx, int dy, int sign_x, int sign_y, int swap,
 			    ei_color_t color,
 			    const ei_rect_t *clipper) {
-	/* TODO: Transparence additionnée */
 	int width = hw_surface_get_size(surface).width;
 	int i, j = 0;
 	int incr_x = sign_x, incr_y = (sign_y) * width; // Parcours des pixels à l'endroit ou non
