1fa5e240b71c1cdd04e1a1302a9b6d75a59deee3
Damien <thecsmine@gmail.com>
2021-05-18 13:39:48 +0200

    Update lines.c

--- tests/lines.c
+++ tests/lines.c
@@ -142,18 +142,20 @@ void test_arc(ei_surface_t surface, ei_rect_t* clipper)
         float rayon = 200; float debut = 0; float fin = 6;
         ei_linked_point_t *pts = malloc(sizeof(*pts));
         pts = arc(centre, rayon, debut, fin);
+	ei_draw_polyline(surface, pts, color, clipper);
         ei_draw_polygon(surface, pts, color, clipper);
 }
 
 void test_rounded_frame	(ei_surface_t surface, ei_rect_t *clipper){
         ei_color_t		color		= { 255, 0, 0, 255 };
-        ei_size_t taille; taille.height = 100; taille.width = 100;
+        ei_size_t taille; taille.height = 200; taille.width = 100;
         ei_point_t pt_rect; pt_rect.x = 100; pt_rect.y = 100;
         ei_rect_t rect; rect.top_left = pt_rect ; rect.size = taille;
         float rayon = 10;
         ei_linked_point_t *pts = malloc(sizeof(*pts));
         pts = rounded_frame(rect, rayon);
-        ei_draw_polygon(surface, pts, color, clipper);
+        ei_draw_polyline(surface, pts, color, clipper);
+        //ei_draw_polygon(surface, pts, color, clipper);
 }
 /*
  * ei_main --
@@ -185,10 +187,10 @@ int main(int argc, char** argv)
 //	test_square	(main_window, clipper_ptr);
 
         /* arc. */
-//	test_arc	(main_window, clipper_ptr);
+	// test_arc	(main_window, clipper_ptr);
 
         /* rounded_frame. */
-        test_rounded_frame	(main_window, clipper_ptr);
+       test_rounded_frame	(main_window, clipper_ptr);
 
 	/* Unlock and update the surface. */
 	hw_surface_unlock(main_window);
