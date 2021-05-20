23f89f23001113e96124214ee919d57002855ca1
Damien <thecsmine@gmail.com>
2021-05-20 14:55:05 +0200

    Version "pas ok"

--- include/ei_placer_utils.h
+++ include/ei_placer_utils.h
@@ -3,18 +3,34 @@
 
 #include "ei_widget.h"
 
+struct anchor_shift {
+	float up_direction;
+	float down_direction;
+	float left_direction;
+	float right_direction;
+};
+
 void init_placer_params(struct ei_widget_t *widget);
 
 ei_bool_t is_valid_padding(float rel_c, int c, int dimension);
 
 ei_bool_t is_valid_coord(int c, int dimension);
 
-void manage_coord_x(ei_widget_t *widget, int* x, float* rel_x);
+void manage_anchor(ei_widget_t *widget, ei_anchor_t *anchor);
 
-void manage_coord_y(ei_widget_t *widget, int* y, float* rel_y);
+void manage_coord_x(ei_widget_t *widget, int *x, float *rel_x);
 
-void manage_width(ei_widget_t *widget, int* width, float* rel_width);
+void manage_coord_y(ei_widget_t *widget, int *y, float *rel_y);
 
-void manage_height(ei_widget_t *widget, int* height, float* rel_height);
+struct anchor_shift init_anchor_shift(float up, float down, float left, float right);
+
+struct anchor_shift create_anchor_shift(ei_anchor_t anchor);
+
+ei_bool_t
+is_valid_dimension(int parent_dimension, float *rel_dimension, int *dimension, int pos_c, float left, float right);
+
+void manage_width(ei_widget_t *widget, int *width, float *rel_width);
+
+void manage_height(ei_widget_t *widget, int *height, float *rel_height);
 
 #endif //EI_PLACER_UTILS_H
