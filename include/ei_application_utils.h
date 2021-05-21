4e0e724c25e7eec1f6181b8da0479fe63db040f3
Damien <thecsmine@gmail.com>
2021-05-21 16:19:03 +0200

    Update ei_application_utils.h

--- include/ei_application_utils.h
+++ include/ei_application_utils.h
@@ -4,14 +4,20 @@
 #include "ei_event.h"
 #include "ei_types.h"
 
+void ei_set_pick_surface(ei_surface_t surface);
+
+ei_surface_t ei_get_pick_surface(void);
+
 int get_quit_number(void);
 
 ei_bool_t is_located_event(ei_event_t event);
 
 ei_bool_t is_quit_event(ei_event_t event);
 
-void draw_root_widget(void);
-
 void draw_widget_recursively(ei_widget_t *widget, ei_surface_t root_window);
+
+void free_widget_recursively(ei_widget_t *widget);
+
+void free_root_window(ei_surface_t root_window);
 
 #endif //EI_APPLICATION_UTILS_H
