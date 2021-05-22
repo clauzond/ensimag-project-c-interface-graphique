1cf08386a61cf1e820470cfb1eaef3647619d139
Damien <thecsmine@gmail.com>
2021-05-22 13:11:08 +0200

    suppression fonctions inutiles

--- src/ei_application_utils.c
+++ src/ei_application_utils.c
@@ -20,18 +20,9 @@ ei_surface_t ei_get_pick_surface(void) {
 	return PICK_SURFACE;
 }
 
-int get_quit_number(void) {
-	return 666;
-}
-
 ei_bool_t is_located_event(ei_event_t event) {
 	return (ei_bool_t) (event.type == ei_ev_mouse_buttondown || event.type == ei_ev_mouse_buttonup ||
 			    event.type == ei_ev_mouse_move);
-}
-
-ei_bool_t is_quit_event(ei_event_t event) {
-	int user_param = (int) event.param.application.user_param;
-	return (ei_bool_t) (user_param == get_quit_number());
 }
 
 void draw_widget_recursively(ei_widget_t *widget, ei_surface_t root_window) {
