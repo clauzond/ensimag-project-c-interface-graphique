94899873bb33aaba1def038c26c40e9fffef7035
Damien <thecsmine@gmail.com>
2021-05-22 13:11:28 +0200

    suppression fonctions inutiles

--- include/ei_application_utils.h
+++ include/ei_application_utils.h
@@ -8,11 +8,7 @@ void ei_set_pick_surface(ei_surface_t surface);
 
 ei_surface_t ei_get_pick_surface(void);
 
-int get_quit_number(void);
-
 ei_bool_t is_located_event(ei_event_t event);
-
-ei_bool_t is_quit_event(ei_event_t event);
 
 void draw_widget_recursively(ei_widget_t *widget, ei_surface_t root_window);
 
