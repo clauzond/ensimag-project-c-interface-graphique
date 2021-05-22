85504438878c2c56012beff26dfbb90b274f315d
Damien <thecsmine@gmail.com>
2021-05-22 21:01:48 +0200

    Update ei_application.c

--- src/ei_application.c
+++ src/ei_application.c
@@ -109,11 +109,13 @@ void ei_app_run() {
 		}
 
 		// Update necessary rectangles
-		hw_surface_lock(ROOT_WINDOW);
-		big_rect = big_union_rect(&RECTANGLE_LIST);
-		draw_widget_recursively(ROOT_FRAME, ROOT_WINDOW, &big_rect);
-		hw_surface_unlock(ROOT_WINDOW);
-		hw_surface_update_rects(ROOT_WINDOW, NULL);
+		if (RECTANGLE_LIST != NULL) {
+			hw_surface_lock(ROOT_WINDOW);
+			big_rect = big_union_rect(&RECTANGLE_LIST);
+			draw_widget_recursively(ROOT_FRAME, ROOT_WINDOW, &big_rect);
+			hw_surface_unlock(ROOT_WINDOW);
+			hw_surface_update_rects(ROOT_WINDOW, NULL);
+		}
 
 		hw_event_wait_next(&event);
 	}
