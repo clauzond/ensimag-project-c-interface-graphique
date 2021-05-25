61a7d0a916c567dbc8f650d4905a6bf278b96a14
Baptiste Bordellier <baptiste.bordellier@genoble-inp.org>
2021-05-25 22:00:21 +0200

    r

--- tests/hello_world.c
+++ tests/hello_world.c
@@ -81,7 +81,7 @@ int main(int argc, char** argv)
 
 	/* Create, configure and place a button as a descendant of the toplevel window. */
 	button = ei_widget_create("button", window, NULL, NULL);
-	ei_button_configure	(button, NULL, &button_color, 
+	ei_button_configure	(button, NULL, &button_color,
 				 &button_border_width, NULL, &button_relief, &button_title, NULL, &button_text_color, NULL,
 				NULL, NULL, NULL, &button_callback, NULL);
 	ei_place(button, &button_anchor, &button_x, &button_y, NULL, NULL, &button_rel_x, &button_rel_y, &button_rel_width, NULL);
